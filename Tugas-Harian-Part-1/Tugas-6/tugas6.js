566700e92aee5b446ffa66ed711ad958f869e09e
idadhamz <dadiilhamsetiadii@gmail.com>
2021-02-23 11:11:50 +0700

    update

--- Tugas-Harian-Part-1/Tugas-6/tugas6.js
+++ Tugas-Harian-Part-1/Tugas-6/tugas6.js
@@ -1,82 +1,82 @@
-
 // Jawaban Soal 1
-let jari_jari;
-let diameter;
-let phi;
+let jari_jari
+let diameter
+let phi
 
 const luasLingkaran = (jari_jari) => {
-    if(jari_jari %7 == 0){
-        phi = 22/7;
-    }else{
-        phi = 3,14;
-    }
+  if (jari_jari % 7 == 0) {
+    phi = 22 / 7
+  } else {
+    ;(phi = 3), 14
+  }
 
-    return phi*jari_jari*jari_jari;
-};
+  return phi * jari_jari * jari_jari
+}
 
 const kelilingLingkaran = (diameter) => {
-    if(diameter %7 == 0){
-        phi = 22/7;
-    }else{
-        phi = 3,14;
-    }
+  if (diameter % 7 == 0) {
+    phi = 22 / 7
+  } else {
+    ;(phi = 3), 14
+  }
 
-    return phi*diameter;
-};
+  return phi * diameter
+}
 
-console.log(luasLingkaran(7));
-console.log(kelilingLingkaran(14));
-console.log("------------");
+console.log(luasLingkaran(7))
+console.log(kelilingLingkaran(14))
+console.log('------------')
 
 // Jawaban Soal 2
-let kalimat = "";
-let kata;
+let kalimat = ''
+let kata
 
 const tambahKata = (kata) => {
-    return kalimat += `${kata} `;
-};
+  return (kalimat += `${kata} `)
+}
 
-tambahKata("saya");
-tambahKata("adalah");
-tambahKata("seorang");
-tambahKata("frontend");
-tambahKata("developer");
+tambahKata('saya')
+tambahKata('adalah')
+tambahKata('seorang')
+tambahKata('frontend')
+tambahKata('developer')
 
-console.log(kalimat);
-console.log("------------");
+console.log(kalimat)
+console.log('------------')
 
 // Jawaban Soal 3
 const newFunction = (firstName, lastName) => {
-    obj = {
-        firstName, lastName, 
-        fullName: () => {
-            return `${obj.firstName} ${obj.lastName}`;
-        }
-    }
-    return obj;
-};
+  obj = {
+    firstName,
+    lastName,
+    fullName: () => {
+      return `${obj.firstName} ${obj.lastName}`
+    },
+  }
+  return obj
+}
 
-console.log(newFunction("William", "Imoh").fullName());
-console.log("------------");
+console.log(newFunction('William', 'Imoh').fullName())
+console.log('------------')
 
 // Jawaban Soal 4
 const actor = {
-    firstName: "Harry",
-    lastName: "Potter Holt",
-    destination: "Hogwarts React Conf",
-    occupation: "Deve-wizard Avocado",
-    spell: "Vimulus Renderus!!!"
+  firstName: 'Harry',
+  lastName: 'Potter Holt',
+  destination: 'Hogwarts React Conf',
+  occupation: 'Deve-wizard Avocado',
+  spell: 'Vimulus Renderus!!!',
 }
 
-const {firstName, lastName, destination, occupation, spell} = actor;
-console.log(firstName, lastName, destination, occupation, spell);
-console.log("------------");
+const { firstName, lastName, destination, occupation, spell } = actor
+console.log(firstName, lastName, destination, occupation, spell)
+console.log('------------')
 
 // Jawaban Soal 5
-const west = ["Will", "Chris", "Sam", "Holly"];
-const east = ["Gill", "Brian", "Noel", "Maggie"];
+const west = ['Will', 'Chris', 'Sam', 'Holly']
+const east = ['Gill', 'Brian', 'Noel', 'Maggie']
 
-let combined = [...west, ...east];
+let combined = [...west, ...east]
 
 console.log(combined)
-console.log("------------");
+console.log('------------')
--- Tugas-Harian-Part-1/Tugas-7/tugas7.js
+++ Tugas-Harian-Part-1/Tugas-7/tugas7.js
@@ -1,100 +1,98 @@
-
 // Jawaban Soal 1
-console.log("----SOAL 1----");
+console.log('----SOAL 1----')
 
-console.log("----RELEASE 0----");
+console.log('----RELEASE 0----')
 
 class Animal {
-    constructor(name, legs = 4, cold_blooded = false){
-        this._name = name;
-        this._legs = legs;
-        this._cold_blooded = cold_blooded;
-    }
+  constructor(name, legs = 4, cold_blooded = false) {
+    this._name = name
+    this._legs = legs
+    this._cold_blooded = cold_blooded
+  }
 
-    get name() {
-        return this._name;
-    }
+  get name() {
+    return this._name
+  }
 
-    set name(x) {
-        this._name = x;
-    }
-
+  set name(x) {
+    this._name = x
+  }
 }
 
-var sheep = new Animal("shaun");
+var sheep = new Animal('shaun')
 
-console.log(sheep.name);
-console.log(sheep._legs);
-console.log(sheep._cold_blooded);
+console.log(sheep.name)
+console.log(sheep._legs)
+console.log(sheep._cold_blooded)
 
-console.log("----RELEASE 1----");
+console.log('----RELEASE 1----')
 
 class Ape extends Animal {
-    constructor(name, legs = 2){
-        super();
-        this._name = name;
-        this._legs = legs;
-    }
+  constructor(name, legs = 2) {
+    super()
+    this._name = name
+    this._legs = legs
+  }
 
-    yell = () => {
-        console.log(`Auooo dan mempunyai kaki ${this._legs}`);
-    }
+  yell = () => {
+    console.log(`Auooo dan mempunyai kaki ${this._legs}`)
+  }
 }
 
 class Frog extends Animal {
-    constructor(legs, name){
-        super(legs);
-        this._name = name;
-    }
+  constructor(legs, name) {
+    super(legs)
+    this._name = name
+  }
 
-    jump = () => {
-        console.log(`hop hop dengan jumlah kaki ${this._legs}`);
-    }
+  jump = () => {
+    console.log(`hop hop dengan jumlah kaki ${this._legs}`)
+  }
 }
 
-var sungokong = new Ape("kera sakti");
-sungokong.yell();
+var sungokong = new Ape('kera sakti')
+sungokong.yell()
 
-var kodok = new Frog("buduk");
-kodok.jump();
+var kodok = new Frog('buduk')
+kodok.jump()
 
 // Jawaban Soal 2
-console.log("----SOAL 2----");
+console.log('----SOAL 2----')
 
 class Clock {
-    constructor({ template }) {
-      this._template = template;
-    }
-  
-    render() {
-      var date = new Date();
-  
-      var hours = date.getHours();
-      if (hours < 10) hours = '0' + hours;
-  
-      var mins = date.getMinutes();
-      if (mins < 10) mins = '0' + mins;
-  
-      var secs = date.getSeconds();
-      if (secs < 10) secs = '0' + secs;
-  
-      var output = this._template
-        .replace('h', hours)
-        .replace('m', mins)
-        .replace('s', secs);
-  
-      console.log(output);
-    }
-  
-    stop() {
-      clearInterval(this.timer);
-    }
-  
-    start() {
-      this.render();
-      this.timer = setInterval(() => this.render(), 1000);
-    }
+  constructor({ template }) {
+    this._template = template
+  }
+
+  render() {
+    var date = new Date()
+
+    var hours = date.getHours()
+    if (hours < 10) hours = '0' + hours
+
+    var mins = date.getMinutes()
+    if (mins < 10) mins = '0' + mins
+
+    var secs = date.getSeconds()
+    if (secs < 10) secs = '0' + secs
+
+    var output = this._template
+      .replace('h', hours)
+      .replace('m', mins)
+      .replace('s', secs)
+
+    console.log(output)
+  }
+
+  stop() {
+    clearInterval(this.timer)
+  }
+
+  start() {
+    this.render()
+    this.timer = setInterval(() => this.render(), 1000)
+  }
 }
 
-var clock = new Clock({template: 'h:m:s'});
-clock.start();
+var clock = new Clock({ template: 'h:m:s' })
+clock.start()
