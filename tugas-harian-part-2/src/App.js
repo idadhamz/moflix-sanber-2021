70b5e22116bf62c853570e912f2246ce119e251d
idadhamz <dadiilhamsetiadii@gmail.com>
2021-02-25 13:57:38 +0700

    tugas-harian-part-2

---
+++ tugas-harian-part-2/src/App.js
@@ -0,0 +1,15 @@
+import './App.css'
+
+import Form from './components/form'
+import Title from './components/title'
+
+function App() {
+  return (
+    <div className="App">
+      <Title />
+      <Form />
+    </div>
+  )
+}
+
+export default App
